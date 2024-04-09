5a8863a1ecfec523e1938584d0832cf3db981882
Lucas Mendes <lucasmendescontact@gmail.com>
2024-04-08 22:10:54 -0300

    Initial commit

---
+++ src/test/java/br/com/lucas/bot/BotDiscordApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotDiscordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
